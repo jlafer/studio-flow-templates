8d7dfd25e03df89901fc3af78d7c54cde2cac85a
John Lafer <jlafer@yahoo.com>
2022-05-10 10:25:49 -0700

    added create and deploy commands

--- src/index.js
+++ src/index.js
@@ -14,13 +14,24 @@ const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 switch (cmd) {
   case 'list':
+    // list 
     listFlows(client);
     break;
   case 'get':
+    // get FWxxxx
     getFlow(client, params[0]);
     break;
   case 'apply':
+    // apply FWxxxx <config-name>
     applyParamsToFlow(client, params[0], params[1]);
+    break;
+  case 'create':
+    // create FWxxxx <config-name>
+    createFlow(client, params[0], params[1]);
+    break;
+  case 'deploy':
+    // deploy FWxxxx <config-name>
+    deployFlow(client, params[0], params[1]);
     break;
   default:
     console.error(`invalid cmd option ${cmd}`);
@@ -46,6 +57,42 @@ async function applyParamsToFlow(client, sid, cust) {
     const json = await fetchFlow(client, sid);
     const newJson = substituteTokensInString(json, custConfig.parameters);
     writeFile(`${sid}_${cust}.json`, newJson, 'utf8');
+  }
+  catch (err) {
+    console.log('error:', err);
+  }
+}
+
+async function createFlow(client, sid, cust) {
+  try {
+    const custJson = await readFile(`${cust}_config.json`, 'utf8');
+    const custConfig = JSON.parse(custJson);
+    const {name, status} = custConfig;
+    const json = await readFile(`${sid}_${cust}.json`, 'utf8');;
+    const flow = await client.studio.flows.create({
+      friendlyName: name,
+      status,
+      definition: json
+    });
+    const {sid: newSid, valid} = flow;
+    console.log(`created flow: sid=${newSid} status=${status} valid=${valid} ${name}`);
+  }
+  catch (err) {
+    console.log('error:', err);
+  }
+}
+
+async function deployFlow(client, sid, cust) {
+  try {
+    const custJson = await readFile(`${cust}_config.json`, 'utf8');
+    const custConfig = JSON.parse(custJson);
+    const {phone} = custConfig;
+    const phoneList = await client.incomingPhoneNumbers.list({phoneNumber: phone});
+    const phoneRsrc = phoneList[0];
+    const {sid: phoneSid} = phoneRsrc;
+    const flowUrl = `https://webhooks.twilio.com/v1/Accounts/${TWILIO_ACCOUNT_SID}/Flows/${sid}`;
+    await client.incomingPhoneNumbers(phoneSid).update({voiceUrl: flowUrl})
+    console.log(`deployed flow: sid=${sid} to phone number ${phone}`);
   }
   catch (err) {
     console.log('error:', err);
